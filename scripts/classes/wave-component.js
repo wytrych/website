a293595c35375c88142a0c9b98891143295433c2
wytrych <fuzzzym@gmail.com>
2017-02-05 21:07:37 +0100

    Change all tabs to spaces

--- scripts/classes/wave-component.js
+++ scripts/classes/wave-component.js
@@ -1,67 +1,67 @@
 export class WaveComponent {
 
-	constructor ({speed, alphaStep, red, green, blue, duration, x, y}, ctx) {
+    constructor ({speed, alphaStep, red, green, blue, duration, x, y}, ctx) {
         this.ctx = ctx
-		this.speed = speed
+        this.speed = speed
 
-		this.red = red
-		this.green = green
-		this.blue = blue
-		this.duration = duration
+        this.red = red
+        this.green = green
+        this.blue = blue
+        this.duration = duration
 
-		this.alpha = 1
-		this.alphaStep = alphaStep
+        this.alpha = 1
+        this.alphaStep = alphaStep
 
-		this.frameCount = 0
-		this.radius = 0
+        this.frameCount = 0
+        this.radius = 0
 
-		this.x = x
-		this.y = y
+        this.x = x
+        this.y = y
 
-		this.defunct = false
+        this.defunct = false
         this.DEFUNCT_LIMIT = 0.0001
 
         this.RADIUS_MODIFIER = 1 / 300
         this.GLOW_PERIOD = 50
-	}
+    }
 
-	spreadStep () {
-		this.increment()
-		this.paint()
-	}
+    spreadStep () {
+        this.increment()
+        this.paint()
+    }
 
-	increment () {
-		this.alpha -= this.alphaStep
-		this.radius += this.speed
+    increment () {
+        this.alpha -= this.alphaStep
+        this.radius += this.speed
 
-		this.frameCount += 1
+        this.frameCount += 1
         this.frameCount %= this.GLOW_PERIOD
 
-		if (this.alpha < this.DEFUNCT_LIMIT)
-			this.defunct = true
-	}
+        if (this.alpha < this.DEFUNCT_LIMIT)
+            this.defunct = true
+    }
 
-	paint () {
-		this.ctx.beginPath()
-		this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+    paint () {
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
 
-		const R = this.red
-		const G = this.green
-		const B = this.blue
-		const A = this.alpha * this.waveFunction(this.frameCount)
+        const R = this.red
+        const G = this.green
+        const B = this.blue
+        const A = this.alpha * this.waveFunction(this.frameCount)
 
         this.currentAlpha = A * this.radius * this.RADIUS_MODIFIER
-		this.ctx.fillStyle =  `rgba(${R}, ${G}, ${B}, ${A})`
-		this.ctx.fill()
-		this.ctx.closePath()
-	}
+        this.ctx.fillStyle =  `rgba(${R}, ${G}, ${B}, ${A})`
+        this.ctx.fill()
+        this.ctx.closePath()
+    }
 
-	waveFunction (position) {
-		const howFarInPeriod = (position / this.GLOW_PERIOD)
-		const modifier = 8
+    waveFunction (position) {
+        const howFarInPeriod = (position / this.GLOW_PERIOD)
+        const modifier = 8
 
-		return (Math.sin(2 * Math.PI * howFarInPeriod) + 1) / modifier
-	}
+        return (Math.sin(2 * Math.PI * howFarInPeriod) + 1) / modifier
+    }
 
     repositionHeight (amount) {
         this.y += amount
--- scripts/classes/wave.js
+++ scripts/classes/wave.js
@@ -2,14 +2,14 @@ import { WaveComponent } from './wave-component'
 import { AudioSignal } from './audio-signal'
 
 export class Wave {
-	constructor (waveSettings, ctx, ENV) {
+    constructor (waveSettings, ctx, ENV) {
         this.ctx = ctx
         this.GAP_MULTIPLIER = 40
 
-		this.numOfWaves = 8
-		this.waves = []
+        this.numOfWaves = 8
+        this.waves = []
 
-		this.waveSettings = waveSettings
+        this.waveSettings = waveSettings
 
         if (!waveSettings.isSilent)
             this.signal = new AudioSignal(waveSettings, ENV)
@@ -17,27 +17,27 @@ export class Wave {
         this.phaseOut = false
         this.defunct = false
 
-		for (let i = 0; i < this.numOfWaves; i++)
-			this.createWave(i)
-	}
+        for (let i = 0; i < this.numOfWaves; i++)
+            this.createWave(i)
+    }
 
-	createWave (i) {
+    createWave (i) {
         // Gap is unsynced with framerate on purpose to add a bit of a random shift
-		const gap = Math.round((i / this.numOfWaves) * this.waveSettings.duration) * this.GAP_MULTIPLIER
-		setTimeout(() => {
+        const gap = Math.round((i / this.numOfWaves) * this.waveSettings.duration) * this.GAP_MULTIPLIER
+        setTimeout(() => {
             if (!this.phaseOut)
                 this.waves[i] = new WaveComponent(this.waveSettings, this.ctx)
-		}, gap)
-	}
+        }, gap)
+    }
 
-	spreadStep () {
+    spreadStep () {
         let summaricAlpha = 0
-		this.waves.forEach((wave, i) => {
-			wave.spreadStep()
+        this.waves.forEach((wave, i) => {
+            wave.spreadStep()
             summaricAlpha += wave.currentAlpha
-			if (wave.defunct)
+            if (wave.defunct)
                 this.replaceOrClearWave(i)
-		})
+        })
 
         if (this.signal)
             this.signal.changeGain(summaricAlpha)
@@ -45,7 +45,7 @@ export class Wave {
         this.waves = this.waves.filter((wave) => wave !== null)
         if (this.phaseOut && this.waves.length === 0)
             this.defunct = true
-	}
+    }
 
     replaceOrClearWave (i) {
         if (this.phaseOut)
